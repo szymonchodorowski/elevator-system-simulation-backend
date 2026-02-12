d880e2577f18945e709d21016fc7c8cb5d0b96db
Szymon Chodorowski <szymon.chodorowski@gmail.com>
2026-02-12 15:18:14 +0100

    project setup

---
+++ src/main/java/com/kyotu/elevator/ElevatorApplication.java
@@ -0,0 +1,13 @@
+package com.kyotu.elevator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElevatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElevatorApplication.class, args);
+	}
+
+}
